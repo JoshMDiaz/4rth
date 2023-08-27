8bcbe148d87096e893f27c82286f720f391f14b8
JoshMDiaz <joshua.michael.diaz@gmail.com>
2023-08-26 22:19:09 -0700

    cleanup, fix routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,54 +1,36 @@
 import { cssBundleHref } from '@remix-run/css-bundle'
 import type { LinksFunction } from '@remix-run/node'
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration
 } from '@remix-run/react'
-import { Tabs } from '@mui/material'
-import LinkTab from '~/components/LinkTab'
-import { useState } from 'react'
+import Tabs from './components/Tabs'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+	...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : [])
 ]
 
 export default function App() {
-  const [value, setValue] = useState(0)
-
-  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue)
-  }
-
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <h1>Skinz</h1>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          textColor="secondary"
-          indicatorColor="secondary"
-          aria-label="secondary tabs example"
-        >
-          <LinkTab label="Players" href="/players" />
-          <LinkTab label="Matchups" href="/matchups" />
-          <LinkTab label="Results" href="/results" />
-        </Tabs>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<head>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='width=device-width,initial-scale=1' />
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<h1>Skinz</h1>
+				<Tabs />
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
