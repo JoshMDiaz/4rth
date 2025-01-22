5c6a8ebdca4a70ca8b23978dec8544f737ab7474
JoshMDiaz <joshua.michael.diaz@gmail.com>
2025-01-21 20:02:44 -0700

    Removing Skinz feature

--- app/components/Winners.tsx
+++ app/components/Winners.tsx
@@ -4,78 +4,60 @@ import '../styles/winners.css'
 import { Paper } from '@mui/material'
 
 type WinnersProps = {
-	players: Player[]
+  players: Player[]
 }
 
 const Winners = ({ players }: WinnersProps): JSX.Element | null => {
-	const [scores] = useScores()
+  const [scores] = useScores()
 
-	const showWinners = (obj: Scores): boolean => {
-		// Check if there are 7 rounds
-		if (Object.keys(obj).length !== 7) {
-			return false
-		}
+  const showWinners = (obj: Scores): boolean => {
+    // Check if there are 7 rounds
+    if (Object.keys(obj).length !== 7) {
+      return false
+    }
 
-		for (const roundKey in obj) {
-			const round = obj[roundKey]
+    for (const roundKey in obj) {
+      const round = obj[roundKey]
 
-			// Check if there are 2 results for each round
-			if (Object.keys(round).length !== 2) {
-				return false
-			}
+      // Check if there are 2 results for each round
+      if (Object.keys(round).length !== 2) {
+        return false
+      }
 
-			for (const matchupKey in round) {
-				if (!round[matchupKey]) {
-					return false // Return false if any value is not present or is false
-				}
-			}
-		}
+      for (const matchupKey in round) {
+        if (!round[matchupKey]) {
+          return false // Return false if any value is not present or is false
+        }
+      }
+    }
 
-		return true // Return true if all checks pass
-	}
+    return true // Return true if all checks pass
+  }
 
-	return players.length === 8 && !!scores && showWinners(scores) ? (
-		<div className='winners-container'>
-			<Winner header='1st' placeIndex={0} players={players} />
-			<Winner header='2nd' placeIndex={1} players={players} />
-			<Winner header='3rd' placeIndex={2} players={players} />
-			<Winner header='4RTH' placeIndex={3} players={players} />
-			<Winner header='Most Skinz' skinz players={players} />
-		</div>
-	) : null
+  return players.length === 8 && !!scores && showWinners(scores) ? (
+    <div className='winners-container'>
+      <Winner header='1st' placeIndex={0} players={players} />
+      <Winner header='2nd' placeIndex={1} players={players} />
+      <Winner header='3rd' placeIndex={2} players={players} />
+      <Winner header='4RTH' placeIndex={3} players={players} />
+    </div>
+  ) : null
 }
 
 export default Winners
 
 type WinnerProps = {
-	header: string
-	players: Player[]
-	placeIndex?: number
-	skinz?: boolean
+  header: string
+  players: Player[]
+  placeIndex?: number
 }
 
-const Winner = ({ header, placeIndex = 0, skinz, players }: WinnerProps) => {
-	const mostSkinzPlayer = players.reduce((maxSkinzPlayer, player) => {
-		if (player.skinz > maxSkinzPlayer.skinz) {
-			return player
-		}
-		return maxSkinzPlayer
-	}, players[0])
-
-	// Find all players with the most skinz
-	const mostSkinzPlayers = players.filter(
-		(player) => player.skinz === mostSkinzPlayer.skinz
-	)
-
-	return (
-		<Paper className='winner'>
-			{skinz && mostSkinzPlayers.length > 0 ? (
-				<h2>{mostSkinzPlayers.map((player) => player.name).join(', ')}</h2>
-			) : players.length > 0 ? (
-				<h2>{players[placeIndex].name}</h2>
-			) : null}
-			<div className='winner-divider' />
-			<span>{header}</span>
-		</Paper>
-	)
+const Winner = ({ header, placeIndex = 0, players }: WinnerProps) => {
+  return (
+    <Paper className='winner'>
+      {players.length > 0 ? <h2>{players[placeIndex].name}</h2> : null}
+      <div className='winner-divider' />
+      <span>{header}</span>
+    </Paper>
+  )
 }
--- app/hooks/usePlayers.tsx
+++ app/hooks/usePlayers.tsx
@@ -1,36 +1,35 @@
 import { useEffect, useState } from 'react'
 
 export interface Player {
-	id: number
-	name: string
-	wins: number
-	skinz: number
-	points: number
-	diff: number
+  id: number
+  name: string
+  wins: number
+  points: number
+  diff: number
 }
 
 export const usePlayers = (): [
-	Player[],
-	(newPlayers: Player[]) => void,
-	boolean
+  Player[],
+  (newPlayers: Player[]) => void,
+  boolean,
 ] => {
-	const [players, setPlayers] = useState<Player[]>([]),
-		[loadingPlayers, setLoadingPlayers] = useState(true)
+  const [players, setPlayers] = useState<Player[]>([]),
+    [loadingPlayers, setLoadingPlayers] = useState(true)
 
-	useEffect(() => {
-		// Load players from localStorage on component mount
-		const savedPlayers = localStorage.getItem('players')
-		if (savedPlayers) {
-			setPlayers(JSON.parse(savedPlayers))
-			setLoadingPlayers(false)
-		}
-	}, [])
+  useEffect(() => {
+    // Load players from localStorage on component mount
+    const savedPlayers = localStorage.getItem('players')
+    if (savedPlayers) {
+      setPlayers(JSON.parse(savedPlayers))
+      setLoadingPlayers(false)
+    }
+  }, [])
 
-	// Update players in both state and localStorage
-	const updatePlayers = (newPlayers: Player[]) => {
-		setPlayers(newPlayers)
-		localStorage.setItem('players', JSON.stringify(newPlayers))
-	}
+  // Update players in both state and localStorage
+  const updatePlayers = (newPlayers: Player[]) => {
+    setPlayers(newPlayers)
+    localStorage.setItem('players', JSON.stringify(newPlayers))
+  }
 
-	return [players, updatePlayers, loadingPlayers]
+  return [players, updatePlayers, loadingPlayers]
 }
